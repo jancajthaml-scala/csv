6e1acaad5f643d608fbf2571d3e2da04e72c5691
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-29 18:43:06 +0200

    shortened code

--- src/main/scala/com/github/jancajthaml/csv/CSV.scala
+++ src/main/scala/com/github/jancajthaml/csv/CSV.scala
@@ -1,50 +1,13 @@
 package com.github.jancajthaml.csv
 
 /**
-  * Recursion immutable based line processing
-  *
-  * @author jan.cajthaml
-  */
-private[jancajthaml] object x {
-
-  /**
-    * Sanify value, deletes trailing characters
-    *
-    * @param value
-    * @return cleaned value
-    */
-  def cleanUp(value: String): String = {
-    //remove double wrapped strings sometime present in csv and trim
-    value.replaceAll("^[\\\"\\\']+|[\\\"\\\']+$", "").trim
-  }
-
-  /**
-    * Walk lines line by line recursively and build Map in each step
-    *
-    * @param lines - array of separator separated strings (data lines)
-    * @param head - separator separated string (header line)
-    * @return List of Maps
-    */
-  def walk(lines: Array[Array[String]], head: Array[String]): List[Map[String, String]] = {
-    if (lines.isEmpty) {
-      List.empty
-    } else {
-      //iterate header and data line simultanelously map header col
-      //to value col. Then recurse to next line
-      (for ((k, v) <- (head zip lines.head)) yield (k -> cleanUp(v))).toMap :: walk(lines.drop(1), head)
-    }
-  }
-
-}
-
-/**
   * read CSV data into List (Row) of Maps (colName -> Colvalue)
   *
   * @author jan.cajthaml
   */
 object read extends ((String, Char, Map[String, String]) => List[Map[String, String]]) {
 
-  import x.{walk}
+  type Pair = Map[String, String]
 
   /**
     * parse and map csv payload to list of key-value'd pairs based on
@@ -55,18 +18,30 @@ object read extends ((String, Char, Map[String, String]) => List[Map[String, Str
     * @param mapper - cols filter/projection Map
     * @return List of Maps
     */
-  def apply(source: String, separator: Char, mapper: Map[String, String]): List[Map[String, String]] = {
+  def apply(source: String, separator: Char, mapper: Pair): List[Pair] = {
     //csv pattern is (any_until_separator,separator)*
-    val csvPattern: String = "(\\\"[^\\\"]+\\\")|[^\\" + separator + "]+"
+    val pattern: String = "(\\\"[^\\\"]+\\\")|[^\\" + separator + "]+"
     //remove non csv lines from source
-    val rows: Array[String] = source.split("[\\r\\n]+").filter(!_.isEmpty()).filter(!_.matches(csvPattern))
+    val rows: Array[String] = source.split("[\\r\\n]+").filter(x => (!x.isEmpty() && !x.matches(pattern)))
     //first line is csv header
     val header: Array[String] = rows.head.split(separator)
     //everything else is csv data
     val lines: Array[Array[String]] = rows.drop(1).map(_.split(separator))
     //based on `mapper` filter get cols to keep, drop others
-    val keepIndexes: Array[Int] = header.zipWithIndex.collect{
-      case(a, b) if mapper.isDefinedAt(a) => b
+    val keepIndexes: Array[Int] = header.zipWithIndex.collect {
+      case (a, b) if mapper.isDefinedAt(a) => b
+    }
+
+    //Sanify value, deletes trailing characters
+    def cleanUp(v: String) = v.replaceAll("^[\\\"\\\']+|[\\\"\\\']+$", "").trim
+
+    //Walk lines line by line recursively and build Map in each step /*@tailrec */
+    def walk(lines: Array[Array[String]], head: Array[String]): List[Pair] = {
+      if (lines.isEmpty) List.empty else {
+        //iterate header and data line simultanelously map header col
+        //to value col. Then recurse to next line
+        (for ((k, v) <- (head zip lines.head)) yield (k -> cleanUp(v))).toMap :: walk(lines.drop(1), head)
+      }
     }
 
     walk(
