224800453fb06c0e446f214966e1d52be426db12
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-29 10:03:56 +0200

    added docs and separater sanitification of value into its own function

--- src/main/scala/com/github/jancajthaml/csv/CSV.scala
+++ src/main/scala/com/github/jancajthaml/csv/CSV.scala
@@ -7,11 +7,31 @@ package com.github.jancajthaml.csv
   */
 private[jancajthaml] object x {
 
+  /**
+    * Sanify value, deletes trailing characters
+    *
+    * @param value
+    * @return cleaned value
+    */
+  def cleanUp(value: String): String = {
+    //remove double wrapped strings sometime present in csv and trim
+    value.replaceAll("^[\\\"\\\']+|[\\\"\\\']+$", "").trim)
+  }
+
+  /**
+    * Walk lines line by line recursively and build Map in each step
+    *
+    * @param lines - array of separator separated strings (data lines)
+    * @param head - separator separated string (header line)
+    * @return List of Maps
+    */
   def walk(lines: Array[Array[String]], head: Array[String]): List[Map[String, String]] = {
     if (lines.isEmpty) {
       List.empty
     } else {
-      (for ((k, v) <- (head zip lines.head)) yield (k -> v.replaceAll("^[\\\"\\\']+|[\\\"\\\']+$", "").trim)).toMap :: walk(lines.drop(1), head)
+      //iterate header and data line simultanelously map header col
+      //to value col. Then recurse to next line
+      (for ((k, v) <- (head zip lines.head)) yield (k -> cleanUp(v)).toMap :: walk(lines.drop(1), head)
     }
   }
 
@@ -26,16 +46,34 @@ object read extends ((String, Char, Map[String, String]) => List[Map[String, Str
 
   import x.{walk}
 
+  /**
+    * parse and map csv payload to list of key-value'd pairs based on
+    * filter/projection `mapper`
+    *
+    * @param source - multiline string of csv payload
+    * @param separator - col separator character
+    * @param mapper - cols filter/projection Map
+    * @return List of Maps
+    */
   def apply(source: String, separator: Char, mapper: Map[String, String]): List[Map[String, String]] = {
-    val condition: String = "(\\\"[^\\\"]+\\\")|[^\\" + separator + "]+"
-    val rows: Array[String] = source.split("[\\r\\n]+") filterNot {_.matches(condition)}
+    //csv pattern is (any_until_separator,separator)*
+    val csvPattern: String = "(\\\"[^\\\"]+\\\")|[^\\" + separator + "]+"
+    //remove non csv lines from source
+    val rows: Array[String] = source.split("[\\r\\n]+") filterNot {_.matches(csvPattern)}
+    //first line is csv header
     val header: Array[String] = rows.head.split(separator)
+    //everything else is csv data
     val lines: Array[Array[String]] = rows.drop(1).map(_.split(separator))
-    val toRemove: Array[Int] = header.zipWithIndex.collect{ case(a, b) if !mapper.isDefinedAt(a) => b}
+    //based on `mapper` filter get cols to keep, drop others
+    val toRemove: Array[Int] = header.zipWithIndex.collect{
+      case(a, b) if mapper.isDefinedAt(a) => b
+    }
 
     walk(
+      //drop header values that are not wanted by `mapper` filter
       lines.map(e => e.zipWithIndex.collect { case(a, b) if !toRemove.contains(b) => a }),
-      header.filterNot({!mapper.isDefinedAt(_)}).map(mapper)
+      //drop data cols that are not wanted by `mapper` filter
+      header.filterNot({mapper.isDefinedAt(_)}).map(mapper)
     )
   }
 
